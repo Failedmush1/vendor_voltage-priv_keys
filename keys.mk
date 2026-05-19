66f07e8241141453c4b494be7bb1f0340a7927f4
Failedmush <anytimewicked@gmail.com>
2026-05-19 00:56:16 +0000

    Update keys.mk and Android.bp for A23-aosp

--- keys.mk
+++ keys.mk
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
 PRODUCT_CERTIFICATE_OVERRIDES := \
@@ -20,10 +19,10 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.devicelock:com.android.devicelock.certificate.override \
     com.android.extservices:com.android.extservices.certificate.override \
     com.android.hardware.authsecret:com.android.hardware.authsecret.certificate.override \
-    com.android.hardware.biometrics.face.virtual:com.android.hardware.biometrics.face.virtual.override \
-    com.android.hardware.biometrics.fingerprint.virtual:com.android.hardware.biometrics.fingerprint.virtual.override \
+    com.android.hardware.biometrics.face.virtual:com.android.hardware.biometrics.face.virtual.certificate.override \
+    com.android.hardware.biometrics.fingerprint.virtual:com.android.hardware.biometrics.fingerprint.virtual.certificate.override \
     com.android.hardware.boot:com.android.hardware.boot.certificate.override \
-    com.android.hardware.cas:com.android.hardware.cas.override \
+    com.android.hardware.cas:com.android.hardware.cas.certificate.override \
     com.android.hardware.contexthub:com.android.hardware.contexthub.certificate.override \
     com.android.hardware.dumpstate:com.android.hardware.dumpstate.certificate.override \
     com.android.hardware.gatekeeper.nonsecure:com.android.hardware.gatekeeper.nonsecure.certificate.override \
@@ -31,7 +30,7 @@ PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.hardware.power:com.android.hardware.power.certificate.override \
     com.android.hardware.rebootescrow:com.android.hardware.rebootescrow.certificate.override \
     com.android.hardware.thermal:com.android.hardware.thermal.certificate.override \
-    com.android.hardware.threadnetwork:com.android.hardware.threadnetwork.override \
+    com.android.hardware.threadnetwork:com.android.hardware.threadnetwork.certificate.override \
     com.android.hardware.uwb:com.android.hardware.uwb.certificate.override \
     com.android.hardware.vibrator:com.android.hardware.vibrator.certificate.override \
     com.android.hardware.wifi:com.android.hardware.wifi.certificate.override \
@@ -83,8 +82,9 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     ServiceConnectivityResources:com.android.connectivity.resources.certificate.override \
     ServiceUwbResources:com.android.uwb.resources.certificate.override \
     ServiceWifiResources:com.android.wifi.resources.certificate.override \
-    WifiDialog:com.android.wifi.dialog.certificate.override
+    WifiDialog:com.android.wifi.dialog.certificate.override \
+    GmscompatLib:gmscompat_lib.certificate.override
 
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
-PRODUCT_EXTRA_RECOVERY_KEYS :=
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/failedmush-priv/keys/releasekey
+PRODUCT_CERTIFICATE_GMSCOMPAT_LIB := vendor/failedmush-priv/keys/gmscompat_lib
 PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := $(dir $(PRODUCT_DEFAULT_DEV_CERTIFICATE))
